40526d747d91e997a9c7232b536afab2b7d121d5
minaje0917 <k050918@naver.com>
2023-08-11 15:44:58 +0900

    :heavy_plus_sign: Dependency :: Add Dependency

--- Project/GOMS-iOS-Admin/Project.swift
+++ Project/GOMS-iOS-Admin/Project.swift
@@ -2,24 +2,6 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 import MyPlugin
 
-/*
-                +-------------+
-                |             |
-                |     App     | Contains GOMSIOSAdmin App target and GOMSIOSAdmin unit-test target
-                |             |
-         +------+-------------+-------+
-         |         depends on         |
-         |                            |
- +----v-----+                   +-----v-----+
- |          |                   |           |
- |   Kit    |                   |     UI    |   Two independent frameworks to share code and start modularising your app
- |          |                   |           |
- +----------+                   +-----------+
-
- */
-
-// MARK: - Project
-
 // Local plugin loaded
 let localHelper = LocalHelper(name: "MyPlugin")
 
@@ -27,6 +9,36 @@ let localHelper = LocalHelper(name: "MyPlugin")
 let project = Project(
     name: "GOMS-IOS-Admin",
     organizationName: "HARIBO",
+    packages: [
+        .remote(
+            url: "https://github.com/RxSwiftCommunity/RxFlow.git",
+            requirement: .upToNextMajor(from: "2.10.0")
+        ),
+        .remote(
+            url: "https://github.com/ReactiveX/RxSwift.git",
+            requirement: .upToNextMajor(from: "6.6.0")
+        ),
+        .remote(
+            url: "https://github.com/SnapKit/SnapKit.git",
+            requirement: .upToNextMajor(from: "5.0.1")
+        ),
+        .remote(
+            url: "https://github.com/devxoul/Then",
+            requirement: .upToNextMajor(from: "2")
+        ),
+        .remote(
+            url: "https://github.com/Moya/Moya.git",
+            requirement: .upToNextMajor(from: "15.0.0")
+        ),
+        .remote(
+            url: "https://github.com/onevcat/Kingfisher.git",
+            requirement: .upToNextMajor(from: "7.0.0")
+        ),
+        .remote(
+            url: "https://github.com/GSM-MSG/GAuthSignin-Swift",
+            requirement: .upToNextMajor(from: "0.0.3")
+        )
+    ],
     targets: [
         Target(
             name: "GOMS-IOS-Admin",
@@ -41,7 +53,17 @@ let project = Project(
                 public: ["Sources/public/A/**", "Sources/public/B/**"],
                 private: "Sources/private/**",
                 project: ["Sources/project/A/**", "Sources/project/B/**"]
-            )
+            ),
+            dependencies: [
+             .package(product: "RxFlow"),
+             .package(product: "RxSwift"),
+             .package(product: "RxCocoa"),
+             .package(product: "SnapKit"),
+             .package(product: "Then"),
+             .package(product: "Moya"),
+             .package(product: "Kingfisher"),
+             .package(product: "GAuthSignin")
+            ]
         )
     ]
 )
