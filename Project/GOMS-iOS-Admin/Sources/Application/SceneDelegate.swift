6f2b98eee89f2162c09b378a88693afc382fc172
minaje0917 <k050918@naver.com>
2023-08-11 16:06:11 +0900

    :zap: Change :: SceneDelegate

--- Project/GOMS-iOS-Admin/Sources/Application/SceneDelegate.swift
+++ Project/GOMS-iOS-Admin/Sources/Application/SceneDelegate.swift
@@ -1,15 +1,19 @@
 import UIKit
+import RxFlow
+import RxCocoa
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-    func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
-        guard let scene = (scene as? UIWindowScene) else { return }
+    var coordinator = FlowCoordinator()
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        let appFlow = AppFlow(window: window)
+        self.coordinator.coordinate(flow: appFlow, with: AppStepper())
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {}
