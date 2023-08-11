88598340f9eec4a5df332f83ddc5a7217d42cd0f
minaje0917 <k050918@naver.com>
2023-08-11 16:00:18 +0900

    :sparkles: Add :: GOMSAdminStep

---
+++ Project/GOMS-iOS-Admin/Sources/Application/Flow/Step/GOMSAdminStep.swift
@@ -0,0 +1,22 @@
+import RxFlow
+import UIKit
+
+enum GOMSAdminStep: Step {
+    
+    // MARK: Auth
+    case introIsRequired
+    case loginWithNumberIsRequired
+    
+    // MARK: TabBar
+    case tabBarIsRequired
+    
+    // MARK: Home
+    case qrocdeIsRequired
+    case outingIsRequired
+    case homeIsRequired
+    case profileIsRequired
+    
+    //MARK: Alert
+    case alert(title: String?, message: String?, style: UIAlertController.Style, actions: [UIAlertAction])
+    case failureAlert(title: String?, message: String?, action: [UIAlertAction] = [])
+}
