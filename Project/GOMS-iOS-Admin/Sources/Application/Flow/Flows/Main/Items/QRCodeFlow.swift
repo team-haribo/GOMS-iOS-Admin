b190a20c710bd41eee6a8ef74f4811f0677a6280
minaje0917 <k050918@naver.com>
2023-08-15 15:04:55 +0900

    :sparkles: Add :: QRCodeFlow

--- Project/GOMS-iOS-Admin/Sources/Application/Flow/Flows/Main/Items/QRCodeFlow.swift
+++ Project/GOMS-iOS-Admin/Sources/Application/Flow/Flows/Main/Items/QRCodeFlow.swift
@@ -1,9 +1,77 @@
-//
-//  QRCodeFlow.swift
-//  GOMS-IOS-Admin
-//
-//  Created by 선민재 on 2023/08/11.
-//  Copyright © 2023 HARIBO. All rights reserved.
-//
+import RxFlow
+import UIKit
+import RxSwift
+import RxCocoa
 
-import Foundation
+struct QRCodeStepper: Stepper{
+    var steps = PublishRelay<Step>()
+
+    var initialStep: Step{
+        return GOMSAdminStep.qrocdeIsRequired
+    }
+}
+
+class QRCodeFlow: Flow {
+
+    var root: Presentable {
+        return self.rootViewController
+    }
+    
+    var stepper = QRCodeStepper()
+    
+    private lazy var rootViewController: UINavigationController = {
+        let viewController = UINavigationController()
+        return viewController
+    }()
+
+    init(){}
+    
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? GOMSAdminStep else { return .none }
+        switch step {
+            
+        case .qrocdeIsRequired:
+            return coordinateToQRCode()
+            
+        case .homeIsRequired:
+            return .one(flowContributor: .forwardToParentFlow(withStep: GOMSAdminStep.homeIsRequired))
+            
+        case .introIsRequired:
+            return .end(forwardToParentFlowWithStep: GOMSAdminStep.introIsRequired)
+            
+        case let .alert(title, message, style, actions):
+            return presentToAlert(title: title, message: message, style: style, actions: actions)
+            
+        case let .failureAlert(title, message, action):
+            return presentToFailureAlert(title: title, message: message, action: action)
+            
+        default:
+            return .none
+        }
+    }
+    
+    private func coordinateToQRCode() -> FlowContributors {
+        let vm = QRCodeViewModel()
+        let vc = QRCodeViewController(vm)
+        self.rootViewController.pushViewController(vc, animated: true)
+        return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: vm))
+    }
+    
+    private func presentToAlert(title: String?, message: String?, style: UIAlertController.Style, actions: [UIAlertAction]) -> FlowContributors {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        actions.forEach { alert.addAction($0) }
+        self.rootViewController.topViewController?.present(alert, animated: true)
+        return .none
+    }
+    
+    private func presentToFailureAlert(title: String?, message: String?, action: [UIAlertAction]) -> FlowContributors {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        if !action.isEmpty {
+            action.forEach(alert.addAction(_:))
+        } else {
+            alert.addAction(.init(title: "확인", style: .default))
+        }
+        self.rootViewController.topViewController?.present(alert, animated: true)
+        return .none
+    }
+}
