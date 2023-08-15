e562fd2f5885864610eee54d202101a17a3fa2fd
minaje0917 <k050918@naver.com>
2023-08-15 15:02:56 +0900

    :sparkles: Add :: HOMEFlow

--- Project/GOMS-iOS-Admin/Sources/Application/Flow/Flows/Main/Items/HomeFlow.swift
+++ Project/GOMS-iOS-Admin/Sources/Application/Flow/Flows/Main/Items/HomeFlow.swift
@@ -6,4 +6,82 @@
 //  Copyright © 2023 HARIBO. All rights reserved.
 //
 
-import Foundation
+import RxFlow
+import UIKit
+import RxCocoa
+import RxSwift
+
+struct HomeStepper: Stepper{
+    var steps = PublishRelay<Step>()
+
+    var initialStep: Step{
+        return GOMSAdminStep.homeIsRequired
+    }
+}
+
+class HomeFlow: Flow {
+
+    var root: Presentable {
+        return self.rootViewController
+    }
+    
+    var stepper = HomeStepper()
+    
+    private lazy var rootViewController: UINavigationController = {
+        let viewController = UINavigationController()
+        return viewController
+    }()
+
+    init(){}
+    
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? GOMSAdminStep else { return .none }
+        switch step {
+        case .outingIsRequired:
+            return .one(flowContributor: .forwardToParentFlow(withStep: GOMSAdminStep.outingIsRequired))
+
+        case .qrocdeIsRequired:
+            return .one(flowContributor: .forwardToParentFlow(withStep: GOMSAdminStep.qrocdeIsRequired))
+
+        case .homeIsRequired:
+            return coordinateToHome()
+            
+        case .introIsRequired:
+            return .end(forwardToParentFlowWithStep: GOMSAdminStep.introIsRequired)
+            
+        case let .alert(title, message, style, actions):
+            return presentToAlert(title: title, message: message, style: style, actions: actions)
+            
+        case let .failureAlert(title, message, action):
+            return presentToFailureAlert(title: title, message: message, action: action)
+            
+        default:
+            return .none
+        }
+    }
+    
+    private func coordinateToHome() -> FlowContributors {
+        let vm = HomeViewModel()
+        let vc = HomeViewController(vm)
+        self.rootViewController.pushViewController(vc, animated: true)
+        return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: vm))
+    }
+    
+    private func presentToAlert(title: String?, message: String?, style: UIAlertController.Style, actions: [UIAlertAction]) -> FlowContributors {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        actions.forEach { alert.addAction($0) }
+        self.rootViewController.topViewController?.present(alert, animated: true)
+        return .none
+    }
+    
+    private func presentToFailureAlert(title: String?, message: String?, action: [UIAlertAction]) -> FlowContributors {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        if !action.isEmpty {
+            action.forEach(alert.addAction(_:))
+        } else {
+            alert.addAction(.init(title: "확인", style: .default))
+        }
+        self.rootViewController.topViewController?.present(alert, animated: true)
+        return .none
+    }
+}
