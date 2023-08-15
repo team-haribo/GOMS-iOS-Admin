86f50e32653aa5b6fc34efac2fc35b3122657a83
minaje0917 <k050918@naver.com>
2023-08-15 15:12:18 +0900

    :sparkles: Add :: TabBarFlow

--- Project/GOMS-iOS-Admin/Sources/Application/Flow/Flows/Main/TabBar/TabBarFlow.swift
+++ Project/GOMS-iOS-Admin/Sources/Application/Flow/Flows/Main/TabBar/TabBarFlow.swift
@@ -1,9 +1,85 @@
-//
-//  TabBarFlow.swift
-//  GOMS-IOS-Admin
-//
-//  Created by 선민재 on 2023/08/11.
-//  Copyright © 2023 HARIBO. All rights reserved.
-//
+import RxFlow
+import UIKit
 
-import Foundation
+final class TabBarFlow: Flow {
+    
+    enum TabIndex: Int {
+        case home = 0
+        case qrCode = 1
+        case outing = 2
+    }
+    
+    var root: Presentable {
+        return self.rootVC
+    }
+    
+    private let rootVC = GOMSAdminTabBarViewController()
+    
+    private var homeFlow = HomeFlow()
+    private var qrCodeFlow = QRCodeFlow()
+    private var outingFlow = OutingFlow()
+    
+    init() {}
+    
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? GOMSAdminStep else {return .none}
+        
+        switch step {
+        case .tabBarIsRequired:
+            return coordinateToTabbar(index: 0)
+            
+        case .qrocdeIsRequired:
+            return coordinateToTabbar(index: 1)
+            
+        case .outingIsRequired:
+            return coordinateToTabbar(index: 2)
+            
+        case .introIsRequired:
+            return .end(forwardToParentFlowWithStep: GOMSAdminStep.introIsRequired)
+            
+        default:
+            return .none
+        }
+    }
+    
+}
+
+private extension TabBarFlow {
+    func coordinateToTabbar(index: Int) -> FlowContributors {
+        Flows.use(
+            homeFlow, qrCodeFlow, outingFlow,
+            when: .ready
+        ) { [unowned self] (root1: UINavigationController,
+                            root2: UINavigationController,
+                            root3: UINavigationController) in
+            let homeItem = UITabBarItem(
+                title: "홈",
+                image: UIImage(named: "unselectedAdminHome.svg"),
+                selectedImage: UIImage(named: "selectedAdminHome.svg")
+            )
+            
+            let qrCodeItem = UITabBarItem(
+                title: "외출하기",
+                image: UIImage(named: "unselectedAdminQRCode.svg"),
+                selectedImage: UIImage(named: "selectedAdminQRCode.svg")
+            )
+            let outingItem = UITabBarItem(
+                title: "외출현황",
+                image: UIImage(named: "unselectedAdminOuting.svg"),
+                selectedImage: UIImage(named: "selectedAdminOuting.svg")
+            )
+            root1.tabBarItem = homeItem
+            root2.tabBarItem = qrCodeItem
+            root3.tabBarItem = outingItem
+            
+            self.rootVC.setViewControllers([root1,root2,root3], animated: true)
+            self.rootVC.selectedIndex = index
+
+        }
+        return .multiple(flowContributors: [
+            .contribute(withNextPresentable: homeFlow, withNextStepper: OneStepper(withSingleStep: GOMSAdminStep.homeIsRequired)),
+            .contribute(withNextPresentable: qrCodeFlow, withNextStepper: OneStepper(withSingleStep: GOMSAdminStep.qrocdeIsRequired)),
+            .contribute(withNextPresentable: outingFlow, withNextStepper: OneStepper(withSingleStep: GOMSAdminStep.outingIsRequired))
+        ])
+    }
+}
