4563f8653ea5717f9d198586b6712dc0f48d71e2
minaje0917 <k050918@naver.com>
2023-08-23 23:45:25 +0900

    :sparkles: Add :: fetchOutingCount case, func

--- Project/GOMS-iOS-Admin/Sources/Presentation/Main/Home/Reactor/HomeReactor.swift
+++ Project/GOMS-iOS-Admin/Sources/Presentation/Main/Home/Reactor/HomeReactor.swift
@@ -11,20 +11,31 @@ class HomeReactor: Reactor, Stepper{
     
     var steps: PublishRelay<Step> = .init()
     
+    let outingProvider = MoyaProvider<OutingServices>()
+    
+    var outingCount: OutingCountResponse = .init(outingCount: 0)
+        
+    let keychain = Keychain()
+    
+    let gomsAdminRefreshToken = GOMSAdminRefreshToken.shared
+    
+    lazy var accessToken = "Bearer " + (keychain.read(key: Const.KeychainKey.accessToken) ?? "")
+    
     // MARK: - Reactor
     
     enum Action {
         case profileButtonDidTap
         case createQRCodeButtonDidTap
         case outingButtonDidTap
+        case fetchOutingCount
     }
     
     enum Mutation {
-        
+        case fetchOutingCount(count: Int)
     }
     
     struct State {
-        
+        var count: Int = 0
     }
     
     // MARK: - Init
@@ -43,7 +54,20 @@ extension HomeReactor {
             return createQRCodeButtonDidTap()
         case .outingButtonDidTap:
             return outingButtonDidTap()
+        case .fetchOutingCount:
+            return fetchOutingCount()
         }
+    }
+}
+
+extension HomeReactor {
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case let .fetchOutingCount(count):
+            newState.count = count
+        }
+        return newState
     }
 }
 
@@ -62,5 +86,38 @@ private extension HomeReactor {
     func outingButtonDidTap() -> Observable<Mutation> {
         self.steps.accept(GOMSAdminStep.outingIsRequired)
         return .empty()
+    }
+    
+    func fetchOutingCount() -> Observable<Mutation> {
+        return Observable.create { observer in
+            self.outingProvider.request(.outingCount(authorization: self.accessToken)) { result in
+                switch result {
+                case let .success(res):
+                    do {
+                        self.outingCount = try res.map(OutingCountResponse.self)
+                    }catch(let err) {
+                        print(String(describing: err))
+                    }
+                    let statusCode = res.statusCode
+                    switch statusCode{
+                    case 200..<300:
+                        observer.onNext(Mutation.fetchOutingCount(
+                            count: self.outingCount.outingCount
+                        ))
+                    case 401:
+                        self.gomsAdminRefreshToken.tokenReissuance()
+                        self.steps.accept(GOMSAdminStep.failureAlert(
+                            title: "오류",
+                            message: "작업을 한 번 더 시도해주세요"
+                        ))
+                    default:
+                        print("ERROR")
+                    }
+                case let .failure(err):
+                    observer.onError(err)
+                }
+            }
+            return Disposables.create()
+        }
     }
 }
