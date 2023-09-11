36badbe69afbc104a0ad5afb421e4b13599bc606
Shin ain <s22028@gsm.hs.kr>
2023-09-11 14:57:57 +0900

    :sparkles: Add :: searchButtonDidTap case

--- Project/GOMS-iOS-Admin/Sources/Presentation/Scene/Main/SearchModal/Reator/SearchModalReactor.swift
+++ Project/GOMS-iOS-Admin/Sources/Presentation/Scene/Main/SearchModal/Reator/SearchModalReactor.swift
@@ -19,10 +19,19 @@ class SearchModalReactor: Reactor, Stepper{
     
     var steps: PublishRelay<Step> = .init()
     
+    let searchModalProvider = MoyaProvider<StudentCouncilServices>(plugins: [NetworkLoggerPlugin()])
+    
+    let keychain = Keychain()
+    
+    let gomsAdminRefreshToken = GOMSAdminRefreshToken.shared
+    
+    lazy var accessToken = "Bearer " + (keychain.read(key: Const.KeychainKey.accessToken) ?? "")
+    
     // MARK: - Reactor
     
     enum Action {
         case resetButtonDidTap
+        case searchButtonDidTap(grade: Int?, classNum: Int?, name: String?, isBlackList: Bool?, authority: String?)
     }
     
     enum Mutation {
@@ -49,6 +58,8 @@ extension SearchModalReactor {
                 .just(.resetSegmentedControls(true)),
                 .just(.resetButtons)
             ])
+        case let .searchButtonDidTap(grade, classNum, name, isBlackList, authority):
+            return searchStudent(grade: grade, classNum: classNum, name: name, isBlackList: isBlackList, authority: authority)
         }
     }
 }
@@ -70,7 +81,39 @@ extension SearchModalReactor {
 
 // MARK: - Method
 private extension SearchModalReactor {
-    
+    func searchStudent(grade: Int?, classNum: Int?, name: String?, isBlackList: Bool?, authority: String?) -> Observable<Mutation> {
+        return Observable.create { observer in
+            self.searchModalProvider.request(.search(authorization: self.accessToken, grade: grade, classNum: classNum, name: name, isBlackList: isBlackList, authority: authority)) { response in
+                switch response {
+                case let .success(result):
+                    let statusCode = result.statusCode
+                    print(self.accessToken)
+                    switch statusCode{
+                    case 200..<300:
+                        print("success")
+                    case 401:
+                        self.gomsAdminRefreshToken.tokenReissuance()
+                    case 403:
+                        self.steps.accept(GOMSAdminStep.failureAlert(
+                            title: "오류",
+                            message: "학생회 계정이 아닙니다.",
+                            action: [.init(title: "확인",style: .default) { _ in
+                                self.steps.accept(GOMSAdminStep.introIsRequired)}
+                                ]
+                            )
+                        )
+                    default:
+                        print("ERROR")
+                    }
+                    observer.onCompleted()
+                case .failure(let err):
+                    print(String(describing: err))
+                    observer.onError(err)
+                }
+            }
+            return Disposables.create()
+        }
+    }
 }
 
 
